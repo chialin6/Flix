f9ea828a4e3f6c24f0446b501d3402b2ed922d4e
Chia-Lin Liu <chialinliu.97@gmail.com>
2020-09-27 17:58:15 -0700

    create detailsViewController

---
+++ Flix/MovieDetailsViewController.swift
@@ -0,0 +1,51 @@
+//
+//  MovieDetailsViewController.swift
+//  Flix
+//
+//  Created by stlp on 9/27/20.
+//  Copyright © 2020 chialin. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+class MovieDetailsViewController: UIViewController {
+
+    // outlets
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var posterView: UIImageView!
+    @IBOutlet weak var backdropView: UIImageView!
+    
+    var movie: [String:Any]!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        print(movie["title"]!)
+        titleLabel.text = movie["title"] as? String
+//        titleLabel.sizeToFit()
+        synopsisLabel.text = movie["overview"] as? String
+        synopsisLabel.sizeToFit()
+        
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: "https://image.tmdb.org/t/p/w185" + posterPath)!
+        posterView.af.setImage(withURL: posterUrl)
+        
+        let backdropPath = movie["backdrop_path"] as! String
+        let backdropUrl = URL(string: "https://image.tmdb.org/t/p/w780" + backdropPath)!
+        backdropView.af.setImage(withURL: backdropUrl)
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Flix/MoviesViewController.swift
+++ Flix/MoviesViewController.swift
@@ -64,14 +64,28 @@ class MoviesViewController: UIViewController, UITableViewDataSource, UITableView
         cell.posterView.af.setImage(withURL: posterUrl)
         return cell
     }
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        // print("loading")
+        // sender: cell being tapped
+        // Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        
+        // pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        detailsViewController.movie = movie
+        
+        // unhighligh the selected cell
+        tableView.deselectRow(at: indexPath, animated: true)
     }
-    */
+    
 
 }
