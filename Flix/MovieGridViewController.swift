796b394db7cb5117e63f859ae2e06248bf8bd061
Chia-Lin Liu <chialin6@uw.edu>
2020-10-02 22:25:48 -0700

    details page of collection view

--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -67,14 +67,24 @@ class MovieGridViewController: UIViewController, UICollectionViewDataSource, UIC
     
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        let cell = sender as! UICollectionViewCell
+        let indexPath = collectionView.indexPath(for: cell)!
+        let movie = movies[indexPath.item]
+        
+        // pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieGridDetailsViewController
+        detailsViewController.movie = movie
+        
+        // unhighligh the selected cell
+        collectionView.deselectItem(at: indexPath, animated: true)
     }
-    */
+    
 
 }
